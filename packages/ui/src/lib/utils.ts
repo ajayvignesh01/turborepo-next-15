09af7ae6d70bb846844841e80339522f17756b89
Ajay <ajayvignesh01@gmail.com>
2024-10-24 00:15:13 -0400

    feat: shadcn ui

---
+++ packages/ui/src/lib/utils.ts
@@ -0,0 +1,6 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from '../../../../node_modules/tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
